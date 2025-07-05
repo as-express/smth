b55f423fd6e8983aced3d2a4491451ef1dc28b89
as-express <aset16006@icloud.com>
2025-07-05 21:41:10 +0500

    base setup

--- e-service/src/main.ts
+++ e-service/src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.NATS,
+    options: {
+      servers: ['nats://nats:4222'],
+    },
+  });
+
+  await app.startAllMicroservices();
 }
+
 bootstrap();
